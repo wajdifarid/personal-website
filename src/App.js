a7994688260e3c3dcf3c8e4b30fd2b68e6caf5d1
Farid <faridwajdi05@gmail.com>
2023-06-19 21:56:16 +0700

    Add google verfication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 export default function ResumeFile() {
   return (
-    <iframe class="pdf-embed-container" src="https://drive.google.com/file/d/1jjaf0Vhq7hPwbcOc3W2rufM1N1jwPJAK/preview" width="100%" height="600" allow="autoplay"></iframe>
+    <iframe title="farid_resume" class="pdf-embed-container" src="https://drive.google.com/file/d/1jjaf0Vhq7hPwbcOc3W2rufM1N1jwPJAK/preview" width="100%" height="600" allow="autoplay"></iframe>
   );
 }
