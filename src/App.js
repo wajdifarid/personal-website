b5d71d3c2a69a2ebcee8a2d5a82f6844ed9827c6
Farid <faridwajdi05@gmail.com>
2023-06-29 21:57:07 +0700

    Update to be personal website rough idea

--- src/App.js
+++ src/App.js
@@ -1,5 +1,174 @@
 export default function ResumeFile() {
-  return (
-    <iframe title="farid_resume" class="pdf-embed-container" src="https://drive.google.com/file/d/1jjaf0Vhq7hPwbcOc3W2rufM1N1jwPJAK/preview" width="100%" height="600" allow="autoplay"></iframe>
-  );
+  return (<div className="w-80 h-[3340px] relative bg-white">
+  <div className="left-[73px] top-[296px] absolute text-zinc-700 text-[20px] font-bold leading-loose">Hi, I’m Farid Wajdi</div>
+  <div className="left-[67px] top-[332px] absolute text-zinc-700 text-[14px] font-normal leading-normal">Full-stack Software Engineer</div>
+  <div className="w-60 left-[40px] top-[364px] absolute text-center text-slate-400 text-[14px] font-normal leading-normal">I like to solve problem and build product which leaves an impact on people lives. Based in Jakarta 🇮🇩</div>
+  <div className="w-[120px] px-4 py-2 left-[166px] top-[452px] absolute bg-sky-500 rounded justify-center items-center gap-2.5 inline-flex">
+    <div className="text-center text-white text-[14px] font-normal leading-normal">Contact Me</div>
+  </div>
+  <div className="w-[120px] px-4 py-2 left-[34px] top-[452px] absolute rounded border border border border border-sky-500 justify-center items-center gap-2.5 inline-flex">
+    <div className="text-center text-sky-500 text-[14px] font-normal leading-normal">See Portfolio</div>
+  </div>
+  <div className="left-[104px] top-[548px] absolute text-zinc-700 text-[20px] font-semibold leading-loose">Experiences</div>
+  <div className="left-[136px] top-[1060px] absolute text-zinc-700 text-[20px] font-semibold leading-loose">Skills</div>
+  <div className="w-[265px] h-[392px] left-[28px] top-[612px] absolute">
+    <div className="left-[154px] top-0 absolute flex-col justify-start items-start gap-1 inline-flex">
+      <img className="w-6 h-6 rounded-sm" src="https://via.placeholder.com/24x24" />
+      <div className="flex-col justify-start items-start flex">
+        <div className="text-zinc-700 text-[12px] font-bold leading-none">Software Engineer</div>
+        <div className="text-center text-zinc-700 text-[12px] font-normal leading-none">Lumina</div>
+        <div className="text-center text-zinc-700 text-[12px] font-normal leading-none">Job Platform</div>
+        <div className="text-center text-slate-400 text-[12px] font-normal leading-none">Jan 2022 - Jun 2023</div>
+      </div>
+    </div>
+    <div className="left-[154px] top-[200px] absolute flex-col justify-start items-start gap-1 inline-flex">
+      <img className="w-6 h-6 rounded-sm" src="https://via.placeholder.com/24x24" />
+      <div className="flex-col justify-start items-start flex">
+        <div className="text-zinc-700 text-[12px] font-bold leading-none">Software Engineer</div>
+        <div className="text-center text-zinc-700 text-[12px] font-normal leading-none">Stoqo</div>
+        <div className="text-center text-zinc-700 text-[12px] font-normal leading-none">Logistics</div>
+        <div className="text-center text-slate-400 text-[12px] font-normal leading-none">Jun - Aug 2019</div>
+      </div>
+    </div>
+    <div className="left-0 top-[100px] absolute flex-col justify-start items-end gap-1 inline-flex">
+      <img className="w-6 h-6 rounded-sm" src="https://via.placeholder.com/24x24" />
+      <div className="flex-col justify-start items-end flex">
+        <div className="text-zinc-700 text-[12px] font-bold leading-none">Software Engineer</div>
+        <div className="text-center text-zinc-700 text-[12px] font-normal leading-none">Shopee</div>
+        <div className="text-center text-zinc-700 text-[12px] font-normal leading-none">Ecommerce</div>
+        <div className="text-center text-slate-400 text-[12px] font-normal leading-none">Jan 2020 - Jun 2022</div>
+      </div>
+    </div>
+    <div className="left-0 top-[300px] absolute flex-col justify-center items-end gap-1 inline-flex">
+      <img className="w-6 h-6 rounded-sm" src="https://via.placeholder.com/24x24" />
+      <div className="flex-col justify-center items-end flex">
+        <div className="text-zinc-700 text-[12px] font-bold leading-none">Software Engineer</div>
+        <div className="text-center text-zinc-700 text-[12px] font-normal leading-none">Gojek</div>
+        <div className="text-center text-zinc-700 text-[12px] font-normal leading-none">Transport</div>
+        <div className="text-center text-slate-400 text-[12px] font-normal leading-none">Jun - Aug 2018</div>
+      </div>
+    </div>
+    <div className="left-[126px] top-[40px] absolute flex-col justify-start items-start inline-flex">
+      <div className="w-3 h-[100px] relative">
+        <div className="w-1 h-[100px] left-[4px] top-0 absolute bg-zinc-200 rounded-tl-lg rounded-tr-lg" />
+        <div className="w-3 h-3 left-0 top-0 absolute bg-sky-500 rounded-full" />
+      </div>
+      <div className="w-3 h-[100px] relative">
+        <div className="w-1 h-[100px] left-[4px] top-0 absolute bg-zinc-200 rounded-tl-lg rounded-tr-lg" />
+        <div className="w-3 h-3 left-0 top-0 absolute bg-sky-500 rounded-full border border-sky-500" />
+      </div>
+      <div className="w-3 h-[100px] relative">
+        <div className="w-1 h-[100px] left-[4px] top-0 absolute bg-zinc-200 rounded-tl-lg rounded-tr-lg" />
+        <div className="w-3 h-3 left-0 top-0 absolute bg-sky-500 rounded-full" />
+      </div>
+      <div className="w-3 h-3 bg-sky-500 rounded-full" />
+    </div>
+  </div>
+  <div className="left-[43.50px] top-[1116px] absolute flex-col justify-start items-start gap-4 inline-flex">
+    <div className="px-4 py-3 rounded-lg border border border border border-slate-100 flex-col justify-center items-center gap-1 flex">
+      <div className="justify-center items-center gap-1 inline-flex">
+        <div className="w-4 h-4 relative" />
+        <div className="text-center text-zinc-700 text-[14px] font-bold leading-normal">Front-End Engineer</div>
+      </div>
+      <div className="w-[200px] text-center text-zinc-700 text-[12px] font-normal leading-none">Javascript, React, CSS</div>
+    </div>
+    <div className="px-4 py-3 rounded-lg border border border border border-slate-100 flex-col justify-center items-center gap-1 flex">
+      <div className="justify-center items-center gap-1 inline-flex">
+        <div className="w-4 h-4 relative" />
+        <div className="text-center text-zinc-700 text-[14px] font-bold leading-normal">Back-End Engineer</div>
+      </div>
+      <div className="w-[200px] text-center text-zinc-700 text-[12px] font-normal leading-none">Golang, Java, PHP</div>
+    </div>
+  </div>
+  <div className="left-[40px] top-[2744px] absolute flex-col justify-start items-center gap-1 inline-flex">
+    <div className="text-zinc-700 text-[20px] font-semibold leading-loose">Contact Me</div>
+    <div className="w-60 text-center text-zinc-700 text-[14px] font-normal leading-normal">I’m available for consultations, collaborations, and coffee! ☕</div>
+  </div>
+  <div className="left-[40px] top-[2852px] absolute flex-col justify-start items-center gap-4 inline-flex">
+    <div className="w-60 px-3 py-2 bg-white rounded border border border border border-slate-100 justify-start items-center gap-2.5 inline-flex">
+      <div className="text-center text-zinc-200 text-[14px] font-normal leading-normal">Full name</div>
+    </div>
+    <div className="w-60 px-3 py-2 bg-white rounded border border border border border-slate-100 justify-start items-center gap-2.5 inline-flex">
+      <div className="text-center text-zinc-200 text-[14px] font-normal leading-normal">Email</div>
+    </div>
+    <div className="w-60 px-3 py-2 bg-white rounded border border border border border-slate-100 justify-start items-center gap-2.5 inline-flex">
+      <div className="text-center text-zinc-200 text-[14px] font-normal leading-normal">Subject</div>
+    </div>
+    <div className="w-60 h-20 px-3 py-2 bg-white rounded border border border border border-slate-100 justify-start items-start gap-2.5 inline-flex">
+      <div className="text-center text-zinc-200 text-[14px] font-normal leading-normal">Message</div>
+    </div>
+    <div className="w-[120px] px-4 py-2 bg-sky-500 rounded justify-center items-center gap-2.5 inline-flex">
+      <div className="text-center text-white text-[14px] font-normal leading-normal">Send Message</div>
+    </div>
+  </div>
+  <div className="left-[119px] top-[1324px] absolute text-zinc-700 text-[20px] font-semibold leading-loose">Portfolio</div>
+  <div className="left-[139px] top-[2268px] absolute text-zinc-700 text-[20px] font-semibold leading-loose">Blog</div>
+  <img className="w-[148px] h-[200px] left-[86px] top-[80px] absolute" src="https://via.placeholder.com/148x200" />
+  <div className="left-[20px] top-[1380px] absolute flex-col justify-start items-center gap-4 inline-flex">
+    <div className="flex-col justify-start items-start flex">
+      <img className="w-[280px] h-[136px] rounded-tl-lg rounded-tr-lg border border border border-slate-100" src="https://via.placeholder.com/280x136" />
+      <div className="px-4 pt-2 pb-4 rounded-bl-lg rounded-br-lg border border border border-slate-100 flex-col justify-start items-start flex">
+        <div className="text-sky-500 text-[14px] font-medium leading-normal">Shopee Barokah</div>
+        <div className="text-zinc-700 text-[12px] font-medium leading-none">Shopee</div>
+        <div className="w-[248px] text-slate-400 text-[12px] font-normal leading-none">Shopee Barokah provides halal-certified products. Help product sales to grow by 200%.</div>
+      </div>
+    </div>
+    <div className="flex-col justify-start items-start flex">
+      <img className="w-[280px] h-[136px] rounded-tl-lg rounded-tr-lg border border border border-slate-100" src="https://via.placeholder.com/280x136" />
+      <div className="px-4 pt-2 pb-4 rounded-bl-lg rounded-br-lg border border border border-slate-100 flex-col justify-start items-start flex">
+        <div className="text-sky-500 text-[14px] font-medium leading-normal">Cari Kerja</div>
+        <div className="text-zinc-700 text-[12px] font-medium leading-none">Lumina</div>
+        <div className="w-[248px] text-slate-400 text-[12px] font-normal leading-none">Lumina helps companies find the best talent. Help grow business partner by 200%.</div>
+      </div>
+    </div>
+    <div className="flex-col justify-start items-start flex">
+      <img className="w-[280px] h-[136px] rounded-tl-lg rounded-tr-lg border border border border-slate-100" src="https://via.placeholder.com/280x136" />
+      <div className="px-4 pt-2 pb-4 rounded-bl-lg rounded-br-lg border border border border-slate-100 flex-col justify-start items-start flex">
+        <div className="text-sky-500 text-[14px] font-medium leading-normal">Gopay Insurance</div>
+        <div className="text-zinc-700 text-[12px] font-medium leading-none">Gojek</div>
+        <div className="w-[248px] text-slate-400 text-[12px] font-normal leading-none">Gopay is the safest payment method. Help companies acquire and retain customer by 200% </div>
+      </div>
+    </div>
+    <div className="w-[120px] px-4 py-2 rounded border border border border border-sky-500 justify-center items-center gap-2.5 inline-flex">
+      <div className="text-center text-sky-500 text-[14px] font-normal leading-normal">See Github</div>
+    </div>
+  </div>
+  <div className="left-[20px] top-[2324px] absolute flex-col justify-start items-center gap-4 inline-flex">
+    <div className="p-4 rounded-lg border border border border border-slate-100 flex-col justify-start items-start gap-1 flex">
+      <div className="w-[248px] text-sky-500 text-[14px] font-medium leading-normal">An algorithm for checking palindrome numbers</div>
+      <div className="text-slate-400 text-[12px] font-normal leading-none">29 June 2023</div>
+    </div>
+    <div className="p-4 rounded-lg border border border border border-slate-100 flex-col justify-start items-start gap-1 flex">
+      <div className="w-[248px] text-sky-500 text-[14px] font-medium leading-normal">The Benefits of Automated Data Capture</div>
+      <div className="text-slate-400 text-[12px] font-normal leading-none">13 June 2023</div>
+    </div>
+    <div className="p-4 rounded-lg border border border border border-slate-100 flex-col justify-start items-start gap-1 flex">
+      <div className="w-[248px] text-sky-500 text-[14px] font-medium leading-normal">Data Transformation 101</div>
+      <div className="text-slate-400 text-[12px] font-normal leading-none">29 June 2023</div>
+    </div>
+    <div className="w-[120px] px-4 py-2 rounded border border border border border-sky-500 justify-center items-center gap-2.5 inline-flex">
+      <div className="text-center text-sky-500 text-[14px] font-normal leading-normal">Visit Blog</div>
+    </div>
+  </div>
+  <div className="h-[136.24px] px-4 py-8 left-0 top-[3204px] absolute bg-white flex-col justify-start items-center gap-5 inline-flex">
+    <div className="justify-start items-center gap-5 inline-flex">
+      <div className="w-8 h-8 relative">
+        <div className="w-8 h-8 left-0 top-0 absolute bg-sky-500 rounded-full" />
+      </div>
+      <div className="w-8 h-8 relative">
+        <div className="w-8 h-8 left-0 top-0 absolute bg-sky-500 rounded-full" />
+      </div>
+      <div className="w-[32.24px] h-[32.24px] relative">
+        <div className="w-[32.24px] h-[32.24px] left-0 top-0 absolute bg-sky-500 rounded-[100px]" />
+      </div>
+    </div>
+    <div className="justify-start items-center gap-2 inline-flex">
+      <div className="text-center text-slate-400 text-[13px] font-light leading-tight">Farid Wajdi 2023. All Rights Reserved.</div>
+    </div>
+  </div>
+  <div className="w-80 p-4 left-0 top-0 absolute bg-white border border-slate-100 justify-between items-center gap-2.5 inline-flex">
+    <div className="text-center text-sky-500 text-[20px] font-black leading-loose">FW</div>
+    <div className="w-6 h-6 relative" />
+  </div>
+</div>);
 }
