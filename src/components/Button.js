6fb7fa92b0224349f0db1a5b013376c6f4befd63
Farid <faridwajdi05@gmail.com>
2023-06-30 19:00:11 +0700

    Add interaction to secondary button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 export function SecondaryButton({ content }) {
     return (
-        <div className="min-w-[120px] min-h-10 px-4 py-2 justify-center items-center inline-flex rounded border border-sky-500 gap-2.5 text-center text-sky-500 text-[14px] font-normal leading-normal">{content}</div>
+        <div className="min-w-[120px] min-h-10 px-4 py-2 justify-center items-center inline-flex rounded border font-normal leading-normal border-sky-500 gap-2.5 text-center text-sky-500 hover:text-white text-[14px] hover:bg-sky-500 hover:cursor-pointer active:bg-sky-700">{content}</div>
     )
 }
 
