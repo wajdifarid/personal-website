b605f0d10795cb2c68f9dae16d0b61d14ee1d661
Farid <faridwajdi05@gmail.com>
2023-06-30 19:03:00 +0700

    Add interaction primary button

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ export function SecondaryButton({ content }) {
 
 export function PrimaryButton({ content }) {
     return (
-        <div className="min-w-[120px] min-h-10 px-4 py-2  bg-sky-500 rounded border border-sky-500 justify-center items-center gap-2.5 text-center text-white text-[14px] font-normal leading-normal">
+        <div className="min-w-[120px] min-h-10 px-4 py-2  bg-sky-500 rounded border border-sky-500 justify-center items-center gap-2.5 text-center text-white text-[14px] font-normal leading-normal hover:bg-sky-700 active:bg-sky-800">
             {content}
         </div>
     )
