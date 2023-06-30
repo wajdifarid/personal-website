3788f2bd54a5b0cf70277cabc40ad60d575ed414
Farid <faridwajdi05@gmail.com>
2023-06-30 14:17:44 +0700

    Fix navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 export default function NavBar({ content }) {
   return (
-    <nav className="bg-white border border-slate-100 flex justify-between mx-2">
+    <nav className="bg-white border border-slate-100 flex justify-between px-2">
       <div className="text-left text-sky-500 text-[20px] font-black leading-loose">FW</div>
       <img src="/assets/material-symbols_menu-rounded.svg" alt="burger menu" />
     </nav>
