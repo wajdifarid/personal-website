d89ad849eff8cfde7c369b0e2a3e69e6089459a4
Farid <faridwajdi05@gmail.com>
2023-06-30 14:40:25 +0700

    Update vertical margin on profile section

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ export default function HomePage() {
         <img className="mx-auto min-h-[200px]" src="https://via.placeholder.com/148x200" alt="Farid"/>
         <div className="mx-auto text-zinc-700 text-[20px] font-bold leading-loose">Hi, I’m Farid Wajdi</div>
         <div className="mx-auto text-zinc-700 text-[14px] font-normal leading-normal">Full-stack Software Engineer</div>
-        <div className="mx-auto w-60 left-[40px] text-center text-slate-400 text-[14px] font-normal leading-normal">I like to solve problem and build product which leaves an impact on people lives. Based in Jakarta 🇮🇩</div>
-        <div className="flex mx-auto">
+        <div className="mx-auto my-2 w-60 left-[40px] text-center text-slate-400 text-[14px] font-normal leading-normal">I like to solve problem and build product which leaves an impact on people lives. Based in Jakarta 🇮🇩</div>
+        <div className="flex mx-auto my-2">
           <div className="mx-2">
           <SecondaryButton content="See Portfolio" />
           </div>
