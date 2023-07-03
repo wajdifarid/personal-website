0396101630ad1febe82efa4f21c6965d787e4c24
Farid <faridwajdi05@gmail.com>
2023-07-03 13:56:47 +0700

    Update desktop notification

--- src/components/DesktopNotificationWIP.js
+++ src/components/DesktopNotificationWIP.js
@@ -14,7 +14,7 @@ export default function DesktopNotificationWIP() {
             <div className="mx-auto h-20 p-4 bg-sky-100 rounded justify-start items-center gap-2 inline-flex">
                 <div className="grow shrink basis-0 flex-col justify-center items-start gap-1 inline-flex">
                     <div className="self-stretch text-zinc-700 text-[14px] font-normal leading-normal">
-                        👋 Hey there! My site is still a work in progress, but your feedback is super valuable! Thanks for your understanding and patience.
+                        👋 Hey there! My site is still a work in progress, but your feedback is super valuable! For best experience please use mobile view. Thanks for your understanding and patience.
                     </div>
                 </div>
                 <img className="w-4 h-4 relative" src="/assets/close-icon.svg" alt="close-icon" onClick={handleClick}/>
