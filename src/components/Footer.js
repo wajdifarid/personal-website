46ff83586ce2376c24a96405fc67a7ff633a658a
Farid <faridwajdi05@gmail.com>
2023-07-03 12:38:27 +0700

    Change footer anchors to text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,19 @@
 export default function Footer() {
   return (
-    <div className="flex flex-col px-4 py-8 bg-slate-100 justify-start items-center gap-5">
-      <div className="justify-start items-center gap-5 inline-flex">
-        <div className="w-8 h-8 relative">
-        <a href="mailto:faridwajdi05@gmail.com" target="_blank" rel="noreferrer">
-          <img className="w-8 h-8 left-0 top-0 absolute bg-sky-500 rounded-full" src="/assets/email-icon.svg" alt="email"/>
+    <div className="flex flex-col px-4 py-8 bg-slate-100 items-center gap-5">
+      <div className="flex w-64 gap-6 items-center justify-center">
+        <a className="text-slate-500 text-[0.875rem] font-medium" href="mailto:faridwajdi05@gmail.com" target="_blank" rel="noreferrer">
+          Email
         </a>
-        </div>
-        <div className="w-8 h-8 relative">
-        <a href="https://www.linkedin.com/in/wajdifarid/" target="_blank" rel="noreferrer">
-          <img className="w-8 h-8 left-0 top-0 absolute bg-sky-500 rounded-full" src="/assets/linkedin-icon.svg" alt="linkedin"/>
+        <a className="text-slate-500 text-[0.875rem] font-medium" href="https://www.linkedin.com/in/wajdifarid/" target="_blank" rel="noreferrer">
+          Linkedin
         </a>
-        </div>
-        <div className="w-[32.24px] h-[32.24px] relative">
-        <a href="https://github.com/wajdifarid" target="_blank" rel="noreferrer">
-          <img className="w-[32.24px] h-[32.24px] left-0 top-0 absolute bg-sky-500 rounded-[100px]" src="/assets/github-icon.svg" alt="github"/>
+        <a className="text-slate-500 text-[0.875rem] font-medium" href="https://github.com/wajdifarid" target="_blank" rel="noreferrer">
+          Github
         </a>
-        </div>
       </div>
       <div className="justify-start items-center gap-2 inline-flex">
-        <img className="min-[16px]" src="/assets/copyright-icon.svg" alt="copyright"/>
+        <img src="/assets/copyright-icon.svg" alt="copyright" />
         <div className="text-center text-slate-400 text-[14px] font-light leading-normal">Farid Wajdi 2023. All Rights Reserved.</div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,7 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      'body': ['"Noto Sans"'],
+      sans: ['"Noto Sans"', ...defaultTheme.fontFamily.sans],
     },
     extend: {
       colors: {
