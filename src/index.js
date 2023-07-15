638d390e7d1755ef38dfa6e32ebd9e45a79dafa1
Farid <faridwajdi05@gmail.com>
2023-07-15 22:27:23 +0700

    Move google analytics config to .env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import TagManager from 'react-gtm-module';
 import "./index.css";
 
 import App from "./App";
+
+const gtmId = process.env.REACT_APP_GTM_ID;
+TagManager.initialize({ gtmId });
 
 const root = createRoot(document.getElementById("root"));
 root.render(
