c1f2b827f496ebcd8629e554f38e776a20311520
Farid <faridwajdi05@gmail.com>
2023-07-16 00:22:13 +0700

    Change navbar on desktop

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import { useState } from "react";
 export default function NavBar() {
   return (
     <nav className="py-4 bg-white border border-slate-100 flex justify-between px-2">
-      <div className="text-left text-sky-500 text-[20px] font-black leading-loose">FW</div>
+      <div className="text-left text-sky-500 text-[20px] font-black leading-loose inline">
+        FW
+      </div>
+      <div className="w-full p-2 hidden sm:block">
+        <div className="flex justify-center gap-8 ">
+          <a href="#experiences">Experiences</a>
+          <a href="#skills">Skills</a>
+          <a href="#portfolio">Portfolio</a>
+          <a href="#blogs">Blogs</a>
+          <a href="#contact">Contact Me</a>
+        </div>
+      </div>
       <HamburgerMenu />
     </nav>
   )
@@ -18,7 +29,7 @@ export function HamburgerMenu() {
 
   return (
     <>
-    <div>
+      <div className="sm:hidden">
         <div className={"fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-80 z-20 " + (isOpen ? "" : "hidden")} onClick={toggleMenu}>
         </div>
         <div className={"fixed shadow-2xl top-0 right-0 w-4/6 h-screen bg-white z-20 transition-transform duration-500 transform " + (isOpen ? "translate-x-0" : "translate-x-full")}>
@@ -35,11 +46,9 @@ export function HamburgerMenu() {
           </div>
         </div>
       </div>
-      <div className="p-2 " onClick={toggleMenu}>
+      <div className="p-2 sm:hidden" onClick={toggleMenu}>
         <img src="/assets/hamburger-menu-icon.svg" alt="hamburger menu" />
       </div>
-      
-
     </>
   );
 }
