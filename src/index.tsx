f5e54ef3e0ebb4e766d61e72c8bf673148b56a35
Farid <faridwajdi05@gmail.com>
2023-08-17 23:38:35 +0700

    Fix type

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import ReactGA from "react-ga4";
 
-ReactGA.initialize(process.env.REACT_APP_GA_ID);
+ReactGA.initialize(process.env.REACT_APP_GA_ID as string);
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
