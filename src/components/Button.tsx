be1ab0fb5e0af59ee3b2bf11a52016fd612dfbd2
Farid <faridwajdi05@gmail.com>
2023-08-17 22:18:37 +0700

    Convert button to typescript

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,16 @@
-export function SecondaryButton({ content }) {
+import React from "react"
+
+type Props = {
+    content: string
+}
+
+export function SecondaryButton({ content }: Props) {
     return (
         <div className="select-none min-w-32 h-10 px-4 py-2 justify-center items-center inline-flex rounded border font-normal leading-normal border-sky-500 gap-2.5 text-center text-sky-500 hover:text-white text-[14px] hover:bg-sky-500 hover:cursor-pointer active:bg-sky-700">{content}</div>
     )
 }
 
-export function PrimaryButton({ content }) {
+export function PrimaryButton({ content }: Props) {
     return (
         <div className="select-none w-32 h-10 px-4 py-2  bg-sky-500 rounded border border-sky-500 justify-center items-center gap-2.5 text-center text-white text-[14px] font-normal leading-normal hover:bg-sky-700 hover:cursor-pointer active:bg-sky-800">
             {content}
