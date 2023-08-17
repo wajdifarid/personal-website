328f1ef0e781cf726c2ad052fd02c605ff0f73e3
Farid <faridwajdi05@gmail.com>
2023-08-17 22:21:53 +0700

    Convert skillcard to typescript

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,8 +1,16 @@
-export default function SkillCard({img, title, content}) {
+import React from "react"
+
+type Props = {
+    img: string,
+    title: string,
+    content: string
+}
+
+export default function SkillCard({ img, title, content }: Props) {
     return (
         <div className="w-60 h-20 px-4 py-3 rounded-lg border border-slate-100 flex-col justify-center items-center gap-1 flex">
             <div className="justify-center items-center gap-1 inline-flex">
-                <img className="w-4 h-4 relative" src={img} alt="skill-icon"/>
+                <img className="w-4 h-4 relative" src={img} alt="skill-icon" />
                 <div className="text-center text-zinc-700 text-[14px] font-bold leading-normal">{title}</div>
             </div>
             <div className="text-center text-slate-500 text-[12px] font-normal leading-none">{content}</div>
