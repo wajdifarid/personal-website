7922abfbde78aa5043cb21780d82d3831200abb7
Farid <faridwajdi05@gmail.com>
2023-08-17 22:17:25 +0700

    Convert blog card to typescript

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+type Props = {
+  title: string,
+  publishDate: string
+}
+
+export default function BlogCard({ title, publishDate }: Props) {
+  return (
+    <div className="p-4 rounded-lg border border-slate-100 flex-col justify-start items-start gap-1 flex">
+      <div className="w-[248px] text-sky-500 text-[14px] font-medium leading-normal">{title}</div>
+      <div className="text-slate-400 text-[12px] font-normal leading-none">{publishDate}</div>
+    </div>
+  )
+}
