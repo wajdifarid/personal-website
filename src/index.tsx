f7d9380fb773354a4ec1f36ad6c352f1d2578af9
Farid <faridwajdi05@gmail.com>
2023-08-17 23:21:57 +0700

    Convert index to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
     <App />
